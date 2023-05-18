0f12feb1fc0d081a6aea74d4a42ad1dbd9b9c2d1
coderick137 <ricardoanj.14@gmail.com>
2023-05-18 17:30:16 -0300

    divide aplicação em componentes

---
+++ src/components/Copyright.tsx
@@ -0,0 +1,15 @@
+export function Copyright() {
+  return (
+    <div className="text-sm leading-relaxed text-gray-200">
+      Feito com 💘 por{' '}
+      <a
+        target="_blank"
+        rel="noreferrer"
+        href="https://www.instagram.com/code_rick_/"
+        className="underline hover:text-gray-100"
+      >
+        @code_rick
+      </a>
+    </div>
+  )
+}
